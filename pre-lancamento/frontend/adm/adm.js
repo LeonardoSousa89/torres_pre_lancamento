12d77b77bb7533ac14b258ceb7f307f924e416d5
Leozin89Python <leozin069@gmail.com>
2021-07-14 17:34:04 -0300

    torre-pre-v~1.1.4

--- pre-lancamento/frontend/adm/adm.js
+++ pre-lancamento/frontend/adm/adm.js
@@ -61,9 +61,141 @@ function consulta() {
                     erro.style.display = 'flex'
                 })
 }
+function enviaDados() {
+        
+       let img = document.getElementById('img').value
+       let descri = document.getElementById('descri').value
+       let checkout = document.getElementById('checkout').value
+
+       if(img == ''      || img == null       ||
+          descri == ''   || descri == null    ||
+          checkout == '' || checkout == null    ) {
+
+                alert('por favor, preencha todos os canpos!')
+
+       }else{
+
+                const url = 'http://localhost:81/insert'
+
+                let data = {
+                    img,
+                    descri,
+                    checkout
+                }
+        
+                const config = {
+                method :'POST',
+                status :201,
+                cache  :'default',
+                body:JSON.stringify(data),
+                headers:{
+                    'content-type':'application/json'
+                },
+                mode :'cors'
+            }
+        
+            fetch(url, config)
+                            .then(response => response.json())
+                            .then(_=> _)
+                            .catch(err => console.log('error:' + err))
+
+       }
+
+    limpar() 
+}
+function obtemDadosPorId() {
+    let id = document.getElementById('id').value
+    
+    if(id == '' || id == null){
+        alert('insira um valor de entrada!')
+        limpar()
+    }else{
+        const url = `http://localhost:81/${id}`
+
+        const config = {
+            method :'GET'
+        }
+
+        fetch(url, config)
+                        .then(response => response.json())
+                        .then(response => {
+                            response.filter((e) => {
+                                
+                                let imgBkd = e.img
+                                let img = document.getElementById('img')
+                                img.value = imgBkd
+
+                                let descriBkd = e.descri
+                                let descri = document.getElementById('descri')
+                                descri.value = descriBkd
+
+                                let chkBkd = e.checkout
+                                let checkout = document.getElementById('checkout')
+                                checkout.value = chkBkd
+                         
+                            })
+                        })
+                        .catch(err => console.log(err)) 
+    }
+}
+function alteraDados() {
+    let id = document.getElementById('id').value
+
+    if(id == '' || id == null ){
+        alert('insira um valor de entrada!')
+        limpar()
+    }else{
+        let img = document.getElementById('img').value
+        let descri = document.getElementById('descri').value
+        let checkout = document.getElementById('checkout').value 
+
+        if(img == ''      || img == null       ||
+           descri == ''   || descri == null    ||
+           checkout == '' || checkout == null    ){
+            alert('por favor, preencha todos os campos!')
+        }else{
+            const url = `http://localhost:81/update/${id}`
+
+            const data = {
+                img,
+                descri,
+                checkout
+            }
+    
+            const config = {
+                method :'PUT',
+                body:JSON.stringify(data),
+                headers :{
+                    'content-type':'application/json'
+                }
+            }
+            fetch(url,config)
+                            .then(response => response.json())
+                            .then(_=> _)
+                            .catch(err => console.log(err))
+        }
+    }
+}
+function deletaDados() {
+    let id = document.getElementById('id').value
+
+    if(id == '' || id == null){
+        alert('insira um valor de entrada!')
+        limpar()
+    }else{
+        const url = `http://localhost:81/delete/${id}`
+        const config = {
+            method :'DELETE'
+        }
+        fetch(url,config)
+                        .then(response => response.json())
+                        .then(_=> _)
+                        .catch(err => console.log(err))
+    }
+}
 function fabricaDados(posicao,e) {
     let ul = document.createElement('ul')
-    ul.style.fontSize   = 'small'
+    ul.style.fontSize   = 'x-small'
     ul.style.fontWeight = 'bold'
     ul.append(e)
     posicao.append(ul)
